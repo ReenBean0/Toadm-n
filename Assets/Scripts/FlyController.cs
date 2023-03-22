1ee964b743d901b1d3826d411536c14b4f69a457
Jackson Turner <JACKSON.TURNER-2020@hull.ac.uk>
2023-03-22 17:06:37 +0000

    Circle of flies at cursor position

--- Assets/Scripts/FlyController.cs
+++ Assets/Scripts/FlyController.cs
@@ -6,16 +6,21 @@ public class FlyController : MonoBehaviour
 {
     [SerializeField] private float rotateSpeed; //degrees per second
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rotateSpeed = 60f;
     }
 
     // Update is called once per frame
     void Update()
     {
         //constant rotation of flies on the z axis
-        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+        this.transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+
+        //captures mouse position
+        Vector3 cursorPosition = Input.mousePosition;
+        //translate mouse position to game world position
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
+        this.transform.position = worldPosition;
     }
 }
