108f6349026d53247d9b3141a90d596e2d5e9456
lamhoyin1234 <54350788+lamhoyin1234@users.noreply.github.com>
2023-03-28 13:44:07 +0100

    apply init rigibody value+draft drag power limiter+draft drag line

--- Assets/Scripts/LeapingController.cs
+++ Assets/Scripts/LeapingController.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class LeapingController : MonoBehaviour
 {
-    float minDistance = 100;//Minimum distance needed to launch a leap
-    float maxDistance = 300;//Maximum distance for max force to lauch the leap
-    float forceScale = 200;//Scale to divide distance to resonable force to leap
+    float minDistance = 1f;//Minimum distance needed to launch a leap
+    float maxDistance = 2f;//Maximum distance for max force to lauch the leap
+    float forceScale = 50;//Scale to multiply distance to resonable force to leap
     bool LeapMode = false, startLeap = false;
     Vector2 mouseOriginPos, mouseCurrentPos;
     Vector3 posChecker, originPos;
-    LineRender lineRender;
+    LineRenderer lineRender;
     GameObject playerInputLine;
     // Start is called before the first frame update
     void Start()
@@ -19,21 +19,36 @@ public class LeapingController : MonoBehaviour
         posChecker = transform.position;
         originPos= transform.position;
         playerInputLine = transform.GetChild(0).gameObject;
-        lineRender = playerInputLine.GetComponent<LineRender>();
+        lineRender = playerInputLine.GetComponent<LineRenderer>();
+        lineRender.SetPosition(0, Vector3.zero);
+        lineRender.SetPosition(1, Vector3.zero);
+        //init rigibody value
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+        GetComponent<Rigidbody>().freezeRotation = true;
     }
     private void OnMouseDown()
     {
         //if player press the toadman
         if (!startLeap)
         {
+            originPos = transform.position;
             LeapMode = true;
         }
     }
-
+    void FixedUpdate()
+    {
+        //set the max power of leap can do
+        float maxSpeed = 10;
+        if (GetComponent<Rigidbody>().velocity.magnitude > maxSpeed)
+        {
+            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized * maxSpeed;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         //Debug.Log("Pos diff" + (posChecker - transform.position));
+        //if the object stop moving, enable checker for next leap
         if (posChecker == transform.position)
         {
             startLeap = false;
@@ -42,20 +57,20 @@ public class LeapingController : MonoBehaviour
         {
             startLeap = true;
         }
+        //if player press the toadman
         if (LeapMode)
         {
-            originPos= transform.position;
             if (Input.GetMouseButtonDown(0))
             {
-                mouseOriginPos = Input.mousePosition;
-                //mouseOriginPos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                //mouseOriginPos = Input.mousePosition;
+                mouseOriginPos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 //Debug.Log("Left click down");
             }
             else if (Input.GetMouseButtonUp(0))
             {
                 //Launch
                 //range between 100~300
-                //Debug.Log(Vector2.Distance(mouseOriginPos, mouseCurrentPos));
+                Debug.Log(Vector2.Distance(mouseOriginPos, mouseCurrentPos));
                 float mouseDistance = Vector2.Distance(mouseOriginPos, mouseCurrentPos);
                 //start launch if drag distance reach minimum accept distance
                 if (mouseDistance > minDistance)
@@ -66,7 +81,8 @@ public class LeapingController : MonoBehaviour
                         //set player drag distance to max if more than acceptable
                         mouseDistance = maxDistance;
                     }
-                    float dragPower = mouseDistance / forceScale;
+                    Debug.Log("mouseDistance="+ mouseDistance);
+                    float dragPower = mouseDistance * forceScale;
                     Vector3 posDiff = mouseOriginPos - mouseCurrentPos;
                     //Debug.Log(posDiff);
                     GetComponent<Rigidbody>().AddForce(posDiff * dragPower);
@@ -75,20 +91,26 @@ public class LeapingController : MonoBehaviour
             }
             else if (Input.GetMouseButton(0))
             {
-                mouseCurrentPos = Input.mousePosition;
-                /*
+                //mouseCurrentPos = Input.mousePosition;
+                
                 mouseCurrentPos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                float maxDist = 5;
+                Vector2 dir = mouseCurrentPos - mouseOriginPos;
+                float dist = Mathf.Clamp(Vector2.Distance(mouseOriginPos, mouseCurrentPos), 0, maxDist);
+                Vector2 newPos = mouseOriginPos + (dir.normalized * dist);
                 lineRender.SetPosition(0, new Vector3(mouseOriginPos.x,mouseOriginPos.y,0));
-                lineRender.SetPosition(1, new Vector3(mouseCurrentPos.x,mouseCurrentPos.y,0));
-                */
+                lineRender.SetPosition(1, new Vector3(newPos.x,newPos.y,0));
+
                 //Debug.Log("Left click holding");
             }
         }
+        //reset position to before leap if fall below platform
         if (transform.position.y < -5)
         {
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             transform.position = originPos;
         }
+        //update position to check if it is moving
         posChecker = transform.position;
     }
 }
