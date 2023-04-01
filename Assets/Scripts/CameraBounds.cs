9fcc34e3166cc0d335196f5e9e0007a3efc0dda8
henrypaul2001 <henryp1.hp@googlemail.com>
2023-04-01 17:31:29 +0100

    Camera controller functionality
    - Camera bounds can be placed in the scene that move the camera to a predetermined camera position when the toad collides with the bounds
    - Target position for camera can be specified in the editor
    - Camera has a target speed. When animation begins, camera will accelerate up to the target speed starting from 0

--- Assets/Scripts/CameraBounds.cs
+++ Assets/Scripts/CameraBounds.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class CameraBounds : MonoBehaviour
 {
-    [SerializeField]
-    Vector3 targetCamPos = new Vector3(0, 0, 0);
+    [SerializeField] Vector3 targetCamPos = new Vector3(0, 0, 0);
+    [SerializeField] GameObject gameManagerObject;
 
-    // Start is called before the first frame update
+    CameraController cameraController;
+
     void Start()
     {
-        
+        cameraController = gameManagerObject.GetComponent<CameraController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 TargetCamPos
     {
-        
+        get { return targetCamPos; }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            cameraController.onCollideWithCameraBounds(this);
+        }
     }
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,28 +7,55 @@ public class CameraController : MonoBehaviour
     [SerializeField] Camera camera;
     [SerializeField] List<GameObject> cameraBounds = new List<GameObject>();
     [SerializeField] GameObject toad;
+    [SerializeField] float targetSpeed;
 
-    Transform toadTransform;
-    List<BoxCollider2D> colliders = new List<BoxCollider2D>();
+    CameraBounds currentBound;
 
     // Start is called before the first frame update
     void Start()
     {
-        toadTransform = toad.GetComponent<Transform>();
-        foreach (GameObject cameraBound in cameraBounds)
+        currentBound = cameraBounds[0].GetComponent<CameraBounds>();
+    }
+
+    // This will be called by the camera bounds scripts when the toad collides with them
+    public void onCollideWithCameraBounds(CameraBounds cameraBounds)
+    {
+        if (cameraBounds != currentBound)
         {
-            colliders.Add(cameraBound.GetComponent<BoxCollider2D>());
+            currentBound = cameraBounds;
+            StartCoroutine(AnimateCameraToPos(cameraBounds.TargetCamPos));
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator AnimateCameraToPos(Vector3 targetPosition)
     {
-    }
+        Debug.Log($"Move camera to: {targetPosition.x}, {targetPosition.y}, {targetPosition.z}");
+        Transform camTransform = camera.GetComponent<Transform>();
 
-    // This will be called by the camera bounds scripts
-    void onCollideWithCameraBounds(GameObject cameraBounds)
-    {
+        // Calculate distance and duration of animation
+        float distance = Vector3.Distance(camTransform.position, targetPosition);
+        float duration = (distance / targetSpeed) * 2;
 
+        float startTime = Time.time;
+        float endTime = startTime + duration;
+
+        while (Time.time < endTime)
+        {
+            // Calculate the current progression of the animation between0 and 1, with
+            // 0 being the start of the animation and 1 being the end of the animation
+            float t = Mathf.InverseLerp(startTime, endTime, Time.time);
+
+            // Adjust movement speed based on animation progression
+            float adjustedSpeed = Mathf.Lerp(0, targetSpeed, Mathf.SmoothStep(0, 1, t));
+
+            // Move camera
+            camTransform.position = Vector3.MoveTowards(camTransform.position, targetPosition, adjustedSpeed * Time.deltaTime);
+
+            // Pause coroutine until next frame
+            yield return null;
+        }
+
+        // Animation complete
+        camTransform.position = targetPosition;
     }
 }
