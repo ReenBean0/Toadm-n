55398b1ff71e115b750d4476b7e9be705ce7e866
henrypaul2001 <henryp1.hp@googlemail.com>
2023-04-30 22:26:29 +0100

    Improved attach to moving object slightly
    - OnCollisionExit means no longer attached instead of before where toad would detach on collision of non moving object
    - Checks if collision or collision parent contains "moving" AND "platform" to determine collisions with a moving platform

--- Assets/Scripts/AttachToMovingObject.cs
+++ Assets/Scripts/AttachToMovingObject.cs
@@ -7,12 +7,26 @@ public class AttachToMovingObject : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name.Contains("moving_lovelygrass"))
+        GameObject obj = collision.gameObject;
+        if ((obj.name.ToLower().Contains("moving") && obj.name.ToLower().Contains("platform")) || 
+            (obj.transform.parent.name.ToLower().Contains("moving") && obj.transform.parent.name.ToLower().Contains("platform")))
         {
-            transform.SetParent(collision.gameObject.transform);
+            transform.SetParent(obj.transform);
+            Debug.Log($"Attached to: {obj.name}");
         }
         else
         {
+            transform.SetParent(null);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        GameObject obj = collision.gameObject;
+        if ((obj.name.ToLower().Contains("moving") && obj.name.ToLower().Contains("platform")) ||
+            (obj.transform.parent.name.ToLower().Contains("moving") && obj.transform.parent.name.ToLower().Contains("platform")))
+        {
+            Debug.Log("Toad detached");
             transform.SetParent(null);
         }
     }
