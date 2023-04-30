77c7b5c755286e9652ee9a2213ccbb516de64f6a
henrypaul2001 <henryp1.hp@googlemail.com>
2023-04-30 17:48:03 +0100

    Added breaking platform prefab and updated other prefabs

---
+++ Assets/Scripts/BreakingPlatform.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakingPlatform : MonoBehaviour
+{
+    [SerializeField] float animIntensity;
+    [SerializeField] float animSpeed;
+    [SerializeField] float secondsUntilBreak;
+    bool breaking;
+    Vector3 startPos;
+
+    public void Start()
+    {
+        breaking = false;
+        startPos = transform.parent.position;
+    }
+
+    public void Update()
+    {
+        if (breaking)
+        {
+            float x = startPos.x + Mathf.Sin(Time.time * animSpeed) * animIntensity;
+            transform.parent.position = new Vector3(x, transform.parent.position.y, transform.parent.position.z);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Toadman")
+        {
+            breaking = true;
+            StartCoroutine(Break());
+        }
+    }
+
+    IEnumerator Break()
+    {
+        yield return new WaitForSecondsRealtime(secondsUntilBreak);
+        Destroy(transform.parent.gameObject);
+    }
+}
