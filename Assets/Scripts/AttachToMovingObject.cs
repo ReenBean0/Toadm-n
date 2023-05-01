d2257803583e8ad9b0f2c3a8081ede67bd3f798b
henrypaul2001 <henryp1.hp@googlemail.com>
2023-05-01 14:14:43 +0100

    Fixed errors in attachtomovingobject

--- Assets/Scripts/AttachToMovingObject.cs
+++ Assets/Scripts/AttachToMovingObject.cs
@@ -8,26 +8,42 @@ public class AttachToMovingObject : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject obj = collision.gameObject;
-        if ((obj.name.ToLower().Contains("moving") && obj.name.ToLower().Contains("platform")) || 
-            (obj.transform.parent.name.ToLower().Contains("moving") && obj.transform.parent.name.ToLower().Contains("platform")))
+        if (obj.name.ToLower().Contains("moving") && obj.name.ToLower().Contains("platform"))
         {
             transform.SetParent(obj.transform);
             Debug.Log($"Attached to: {obj.name}");
         }
         else
         {
-            transform.SetParent(null);
+            if (obj.transform.parent != null)
+            {
+                if (obj.transform.parent.name.ToLower().Contains("moving") && obj.transform.parent.name.ToLower().Contains("platform"))
+                {
+                    transform.SetParent(obj.transform);
+                    Debug.Log($"Attached to: {obj.name}");
+                }
+            }
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         GameObject obj = collision.gameObject;
-        if ((obj.name.ToLower().Contains("moving") && obj.name.ToLower().Contains("platform")) ||
-            (obj.transform.parent.name.ToLower().Contains("moving") && obj.transform.parent.name.ToLower().Contains("platform")))
+        if (obj.name.ToLower().Contains("moving") && obj.name.ToLower().Contains("platform"))
         {
             Debug.Log("Toad detached");
             transform.SetParent(null);
+        }
+        else
+        {
+            if (obj.transform.parent != null)
+            {
+                if (obj.transform.parent.name.ToLower().Contains("moving") && obj.transform.parent.name.ToLower().Contains("platform"))
+                {
+                    Debug.Log("Toad detached");
+                    transform.SetParent(null);
+                }
+            }
         }
     }
 }
