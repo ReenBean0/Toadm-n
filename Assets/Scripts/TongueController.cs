6fe8b1f596e82d0f581767a4db856e8e61ff9531
Lam Ho Yin <lamlamhohoyinyin@hotmail.com>
2023-05-03 00:23:40 +0100

    add modify to max distance and animation speed delete old method of animation

--- Assets/Scripts/TongueController.cs
+++ Assets/Scripts/TongueController.cs
@@ -11,7 +11,6 @@ public class TongueController : MonoBehaviour
 {
     [SerializeField] GameObject flyCursor;
     [SerializeField] GameObject tonguePrefab;
-    [SerializeField] float animationSpeed;
     [SerializeField] float animationPauseBeforeReversing;
 
     // Serialized so the cooldown status can be seen in the editor
@@ -21,12 +20,13 @@ public class TongueController : MonoBehaviour
     float flyX;
     float flyY;
     float tongueWidth = 0.4f;
-    float tongueSpeed = 5;
+    [Range(0.0f, 10.0f)]
+    public float tongueSpeed = 2;
     Color tongueColor = new Color(1, 0.47f, 0.47f);
     GameObject tongueObject;
     LineRenderer lineRender;
-
-    GameObject tongueInstance;
+    [Range(0.0f, 10.0f)]
+    public float maxDistance;
 
     //added for moveable objects by Yin
     bool reverse = true;
@@ -49,30 +49,16 @@ public class TongueController : MonoBehaviour
         lineRender.material = new Material(Shader.Find("Sprites/Default"));
         lineRender.startColor = tongueColor;
         lineRender.endColor = tongueColor;
-        //tongueInstance = Instantiate(tonguePrefab, new Vector3(transform.position.x, transform.position.y, 1), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (tongueInstance != null)
+        if (!tongueCooldown)
         {
-            //keep tongue attach to toad
-            lineRender.SetPosition(0, Vector3.zero);
-            slope = (flyY - transform.position.y) / (flyX - transform.position.x);
-            c = flyY - slope * flyX;
-            if (tongueCooldown)
-            {
-                //tongue had shoot
-                tongueInstance.transform.position = transform.position;
-            }
+            lineRender.SetPosition(1, transform.position);
         }
-        */
-        if (tongueCooldown)
-        {
-            lineRender.SetPosition(0, transform.position);
-        }
+        lineRender.SetPosition(0, transform.position);
         if (Input.GetKeyDown(KeyCode.Space) && !tongueCooldown)
         {
             flyX = flyTransform.position.x;
@@ -83,9 +69,6 @@ public class TongueController : MonoBehaviour
             //Vector3 mousePosition = Input.mousePosition;
             //mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             //Debug.Log($"Launch tongue at X={mousePosition.x}, y={mousePosition.y}");
-
-            //Vector3 tongueDestination = new Vector3(flyX, flyY, 1);
-            //LaunchTongue(tongueDestination);
 
             StartCoroutine(ShootTongue());
         }
@@ -98,12 +81,18 @@ public class TongueController : MonoBehaviour
         while (tongueCooldown)
         {
             //launch
-            while (lineRender.GetPosition(1) != targetPos)
+            while (lineRender.GetPosition(1) != targetPos|| Vector3.Distance(lineRender.GetPosition(0), lineRender.GetPosition(1))<maxDistance)
             {
                 targetPos = new Vector3(flyX, flyY, 1);
                 float t = (Time.time - startTime) * tongueSpeed;
                 Vector3 newPos = Vector3.Lerp(transform.position, targetPos, t);
                 lineRender.SetPosition(1, newPos);
+                yield return null;
+            }
+            //Debug.Log("Distance:"+Vector3.Distance(lineRender.GetPosition(0), lineRender.GetPosition(1)));
+            while (!reverse)
+            {
+                //if touch a moveable object
                 yield return null;
             }
             startTime = Time.time;
@@ -118,79 +107,6 @@ public class TongueController : MonoBehaviour
             tongueCooldown = false;
         }
     }
-    void LaunchTongue(Vector3 targetPos)
-    {
-        // Spawn tongue
-        //tongueInstance = Instantiate(tonguePrefab, new Vector3(transform.position.x, transform.position.y, 1), Quaternion.identity);
-
-        // Rotate tongue to face target point
-        Vector3 direction = targetPos - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        tongueInstance.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        // Stretch tongue (multiplied by two because of the size of the prefab)
-        float distance = Vector2.Distance(tongueInstance.transform.position, targetPos) * 2;
-        StartCoroutine(AnimateTongue(tongueInstance, distance));
-    }
-
-    IEnumerator AnimateTongue(GameObject tongue, float distance)
-    {
-        // Enable cooldown so tongue can't be spammed
-        tongueCooldown = true;
-
-        // Set initial scale of tongue
-        tongue.transform.localScale = new Vector3(0.1f, 1, 1);
-
-        // Scale tongue until target size is reached
-        while (tongue.transform.localScale.x < distance)
-        {
-            // Calculate new scale based on elapsed time and speed
-            float newScale = tongue.transform.localScale.x + (animationSpeed * Time.deltaTime);
-            tongue.transform.localScale = new Vector3(newScale, 1, 1);
-
-            // Pause coroutine until next frame
-            yield return null;
-        }
-
-        // If here - animation is complete
-        // Set final scale of tongue
-        tongue.transform.localScale = new Vector3(distance, 1, 1);
-
-        // Wait before reversing animation
-        yield return new WaitForSeconds(animationPauseBeforeReversing);
-        //yield return StartCoroutine(AnimateTongueReverse(tongue));
-        //added for moveable objects by Yin
-        while (!reverse)
-        {
-            //if touch a moveable object
-            yield return null;
-        }
-        yield return StartCoroutine(AnimateTongueReverse(tongue));
-    }
-
-    IEnumerator AnimateTongueReverse(GameObject tongue)
-    {
-        float reversedSpeed = -animationSpeed;
-
-        // Scale tongue until target size is reached
-        while (tongue.transform.localScale.x > 0)
-        {
-            // Calculate new scale based on elapsed time and speed
-            float newScale = tongue.transform.localScale.x + (reversedSpeed * Time.deltaTime);
-            tongue.transform.localScale = new Vector3(newScale, 1, 1);
-
-            // Pause coroutine until next frame
-            yield return null;
-        }
-
-        // Set final scale of tongue
-        tongue.transform.localScale = new Vector3(0.1f, 1, 1);
-
-        // Be gone foul tongue
-        Destroy(tongue);
-        tongueCooldown = false;
-    }
-
     //added for moveable objects by Yin
     public void TriggerMoveableObject()
     {
